222be7dd357e134c876d82be44bed102e8532342
David Iljuchin <david.iljuchin@gmail.com>
2021-10-26 22:42:56 +0200

    inserted error message if there is a negative number

--- fibonacci.py
+++ fibonacci.py
@@ -9,12 +9,12 @@ def fib(n):
     fib_2 = 1
     fib_next = 0
     i = 0
-    if n == 1:
+    if n == 0:
         fib_n = fib_1
-    if n == 2:
+    if n == 1:
         fib_n = fib_2
-    if n > 2:
-        while i != n-2:
+    if n > 1:
+        while i != n-1:
             fib_next = fib_1 + fib_2
             fib_1 = fib_2
             fib_2 = fib_next
@@ -22,11 +22,16 @@ def fib(n):
         fib_n = fib_next
     return fib_n
 
-if n == 1:
+if n == 0:
+    ordinal_number = ""
+elif n == 1:
     ordinal_number = "st"
 elif n == 2:
     ordinal_number = "nd"
 elif n == 3:
     ordinal_umber = "rd"
 
-print("The "+ str(n) + ordinal_number + " number in the fibonacci row is "+ str(fib(n)))
+if n >= 0:
+    print("The "+ str(n) + ordinal_number + " number in the fibonacci row is "+ str(fib(n))+"!")
+else:
+    print("there is no negative number in the fibonacci row!")
