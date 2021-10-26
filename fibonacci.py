243a42943655d328d4f90ea66724874eda77aaf4
David Iljuchin <david.iljuchin@gmail.com>
2021-10-26 14:54:21 +0200

    refactored to single return of fib_n

--- fibonacci.py
+++ fibonacci.py
@@ -7,15 +7,15 @@ def fib(n):
     fib_2 = 1
     fib_next = 0    
     if n == 1:
-        return fib_1
+        fib_n = fib_1
     if n == 2:
-        return fib_2
+        fib_n = fib_2
     if n > 2:
         for i in range(n-2):
             fib_next = fib_2+fib_1
             fib_1 = fib_2
             fib_2 = fib_next
         fib_n = fib_next
-        return fib_n
+    return fib_n
 
 print(fib(n))
