3b0398692b1eb51a89f3fa31b6f202d4acc1e5b3
David Iljuchin <david.iljuchin@gmail.com>
2021-10-26 15:22:28 +0200

    replaced for-loop with while-loop

--- fibonacci.py
+++ fibonacci.py
@@ -5,17 +5,19 @@ n = int(input('wie viele Zahlen sollen angegeben werden?'))
 def fib(n):
     fib_1 = 0
     fib_2 = 1
-    fib_next = 0    
+    fib_next = 0
+    i = 0
     if n == 1:
         fib_n = fib_1
     if n == 2:
         fib_n = fib_2
     if n > 2:
-        for i in range(n-2):
-            fib_next = fib_2+fib_1
+        while i != n-2:
+            fib_next = fib_1 + fib_2
             fib_1 = fib_2
             fib_2 = fib_next
+            i += 1
         fib_n = fib_next
     return fib_n
-
+    
 print(fib(n))
