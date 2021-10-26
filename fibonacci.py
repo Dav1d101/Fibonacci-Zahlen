e2b4c10f450d9e3c725d114ac07149c797df3077
David Iljuchin <david.iljuchin@gmail.com>
2021-10-26 14:46:07 +0200

    converted to method

--- fibonacci.py
+++ fibonacci.py
@@ -1,16 +1,21 @@
+#!/usr/bin/python3
 
 n = int(input('wie viele Zahlen sollen angegeben werden?'))
 
-fib_1 = 0
-fib_2 = 0
-fib_next = 0    
-if n >= 1:
-	print(fib_1)
-if n >= 2:
-	print(fib_2)
-if n > 2:
-    for i in range(n-2):
-        fib_next = fib_2+fib_1
-        fib_1 = fib_2
-        fib_2 = fib_next
-        print(fib_next)
+def fib(n):
+    fib_1 = 0
+    fib_2 = 1
+    fib_next = 0    
+    if n == 1:
+        return fib_1
+    if n == 2:
+        return fib_2
+    if n > 2:
+        for i in range(n-2):
+            fib_next = fib_2+fib_1
+            fib_1 = fib_2
+            fib_2 = fib_next
+        fib_n = fib_next
+        return fib_n
+
+print(fib(n))
