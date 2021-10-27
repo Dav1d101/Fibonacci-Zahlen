9a6ab90312b3560ce64daa10af03f77c0e69717b
David Iljuchin <david.iljuchin@gmail.com>
2021-10-27 12:42:15 +0200

    implimented recursion version

--- fibonacci.py
+++ fibonacci.py
@@ -4,27 +4,27 @@ print('Wich number do you want to know?')
 n = int(input())
 ordinal_number = "th"
 
-def fib(n):
-    fib_1 = 0
-    fib_2 = 1
-    fib_next = 0
-    i = 0
-    if n == 0:
+def fib(n, fib_1, fib_2):
+    fib_n = 0
+    if n == 0 and fib_1 == 0:
         fib_n = fib_1
-    elif n == 1:
+        return fib_1
+    elif n == 1 and fib_2 == 1:
         fib_n = fib_2
-    elif n > 1:
-        while i != n-1:
-            fib_next = fib_1 + fib_2
-            fib_1 = fib_2
-            fib_2 = fib_next
-            i += 1
-        fib_n = fib_next
+        return fib_2
+    elif n < 0:
+        return -1
+    fib_n = fib_1 + fib_2
+    fib_1 = fib_2
+    fib_2 = fib_n
+    if n-2 != 0:
+        n -= 1
+        return fib(n, fib_1, fib_2)
     else:
-        return -1    #returns -1 if n is a negative number
-    return fib_n
+        return fib_n
+    
 
-fib_number = fib(n)
+fib_number = fib(n ,0 ,1)
 if n == 0:
     ordinal_number = ""
 elif n == 1:
@@ -32,7 +32,7 @@ elif n == 1:
 elif n == 2:
     ordinal_number = "nd"
 elif n == 3:
-    ordinal_umber = "rd"
+    ordinal_number = "rd"
 if fib_number >= 0:
     print("The "+ str(n) + ordinal_number + " number in the fibonacci row is "+ str(fib_number)+"!")
 else:
